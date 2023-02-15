2e051d11f60501ba7264ec5e5098f8031a178f76
Matias <alumno.532989@ies-azarquiel.es>
2023-02-15 21:19:36 +0100

    Conf Inicial (Array kiwi)

---
+++ src/main/java/com/cursoentregar/cursoentregar/CursoentregarApplication.java
@@ -0,0 +1,13 @@
+package com.cursoentregar.cursoentregar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoentregarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoentregarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cursoentregar/cursoentregar/CursoentregarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursoentregar.cursoentregar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoentregarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
