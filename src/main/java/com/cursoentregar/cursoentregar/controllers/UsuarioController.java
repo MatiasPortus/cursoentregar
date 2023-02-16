04b83beb26072235c290895f9bf7fbfd093e6d30
Matias <alumno.532989@ies-azarquiel.es>
2023-02-16 08:27:07 +0100

    Justo antes de BD (1:55:00)

--- src/main/java/com/cursoentregar/cursoentregar/controllers/UsuarioController.java
+++ src/main/java/com/cursoentregar/cursoentregar/controllers/UsuarioController.java
@@ -1,15 +1,85 @@
 package com.cursoentregar.cursoentregar.controllers;
 
+import com.cursoentregar.cursoentregar.models.Usuario;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class UsuarioController {
 
-    @RequestMapping(value = "prueba")
-    public List<String> prueba() {
-        return List.of("Manzana", "Kiwi", "Banana");
+    @RequestMapping(value = "usuario/{id}")
+    public Usuario getUsuario(@PathVariable Long id) {
+        Usuario u = new Usuario();
+        u.setId(id);
+        u.setNombre("Matias");
+        u.setApellido("Portus");
+        u.setEmail("matiasportus@gmail.com");
+        u.setTelefono("601223205");
+        return u;
+    }
+
+    @RequestMapping(value = "usuarios")
+    public List<Usuario> getUsuarios() {
+        List<Usuario> usuarios = new ArrayList<>();
+        Usuario u = new Usuario();
+        u.setId(234L);
+        u.setNombre("Matias");
+        u.setApellido("Portus");
+        u.setEmail("matiasportus@gmail.com");
+        u.setTelefono("601223205");
+
+        Usuario u2 = new Usuario();
+        u2.setId(432L);
+        u2.setNombre("Mat");
+        u2.setApellido("Por");
+        u2.setEmail("matpor@gmail.com");
+        u2.setTelefono("601052232");
+
+        Usuario u3 = new Usuario();
+        u3.setId(342L);
+        u3.setNombre("m");
+        u3.setApellido("p");
+        u3.setEmail("mp@gmail.com");
+        u3.setTelefono("620122305");
+
+        usuarios.add(u);
+        usuarios.add(u2);
+        usuarios.add(u3);
+
+        return usuarios;
+    }
+
+    @RequestMapping(value = "usuario1")
+    public Usuario editar() {
+        Usuario u = new Usuario();
+        u.setNombre("Matias");
+        u.setApellido("Portus");
+        u.setEmail("matiasportus@gmail.com");
+        u.setTelefono("601223205");
+        return u;
+    }
+
+    @RequestMapping(value = "usuario2")
+    public Usuario eliminar() {
+        Usuario u = new Usuario();
+        u.setNombre("Matias");
+        u.setApellido("Portus");
+        u.setEmail("matiasportus@gmail.com");
+        u.setTelefono("601223205");
+        return u;
+    }
+
+    @RequestMapping(value = "usuario3")
+    public Usuario buscar() {
+        Usuario u = new Usuario();
+        u.setNombre("Matias");
+        u.setApellido("Portus");
+        u.setEmail("matiasportus@gmail.com");
+        u.setTelefono("601223205");
+        return u;
     }
 }
